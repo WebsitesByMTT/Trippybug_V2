186bd0838a6a0253bca696c426f3d5c56bd10845
TrippyGaurav <gaurav.trippybug@gmail.com>
2024-02-13 10:18:04 +0530

    start search box

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./hero.module.scss";
 import Navbar from "../navbar/Navbar";
 import TimelineMap from "../SVG/TimelineMap";
+import LocationMarker from "../SVG/LocationMarker";
 
 const Hero = () => {
   return (
@@ -32,7 +33,54 @@ const Hero = () => {
                 </div>
               </div>
             </div>
-            <div className={styles.bottom}></div>
+            <div className={styles.bottom}>
+              <div className={styles[`timeline`]}>
+                <div className={styles.points}>
+                  <LocationMarker />
+                  <span>Yousmarg</span>
+                </div>
+                <div className={styles.points}>
+                  <LocationMarker />
+                  <span>Strawberry Valley</span>
+                </div>
+
+                <div className={styles.points}>
+                  <LocationMarker />
+                  <span>Pari Mahal</span>
+                </div>
+
+                <div className={styles.map}>
+                  <TimelineMap className={styles.map} />
+                </div>
+              </div>
+            </div>
+
+            <div className={styles[`search-box`]}>
+              <div className={styles.tabs}>
+                <ul>
+                  <li>Flights</li>
+                  <li>Hotels</li>
+                  <li>Cars</li>
+                </ul>
+              </div>
+              <div className={styles[`search-box-container`]}>
+                <div className={styles.items}>
+                  <input type="text" placeholder="From" />
+                </div>
+                <div className={styles.items}>
+                  <input type="text" placeholder="To" />
+                </div>
+                <div className={styles.items}>
+                  <input type="text" placeholder="Date" />
+                </div>
+                <div className={styles.items}>
+                  <input type="text" placeholder="Passengers" />
+                </div>
+                <div className={styles.items}>
+                  <button>Search</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
