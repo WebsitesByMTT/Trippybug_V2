370b675d8248b94a7e0bed672955213ba7cc2cc4
TrippyRahul <rahulrawat.trippybug@gmail.com>
2024-02-14 10:49:35 +0530

    add trending

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.scss";
 import Hero from "@/components/hero/Hero";
 import ExploreNow from "@/components/exploreNow/ExploreNow";
+import TrendingNow from "@/components/trendingNow/TrendingNow";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Hero />
       <ExploreNow />
+      <TrendingNow />
     </main>
   );
 }
